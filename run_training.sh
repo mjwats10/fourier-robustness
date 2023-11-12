d6063858992fdbc94d9d3b0ef64cff76ff6ae2fc
Matt Watson <mjwats10@asu.edu>
2023-11-11 18:09:00 -0700

    mnist_noise results

--- run_training.sh
+++ run_training.sh
@@ -2,6 +2,9 @@
 
 path='/home/matt/fourier'
 device='cuda:0'
+terms_mlp=5
+terms_gnn=20
+gnn_width=0.25
 
 cd $path
 source env/bin/activate
@@ -12,21 +15,21 @@ do
     python -u -W ignore::UserWarning code/models/mnist_aug_cnn.py $path $device $seed | tee logs/mnist_aug_cnn_s$seed.txt
     python -u -W ignore::UserWarning code/models/mnist_aug_mispredict_cnn.py $path $device $seed | tee logs/mnist_aug_mispredict_cnn_s$seed.txt
     python -u -W ignore::UserWarning code/models/mnist_fourier_cnn.py $path $device $seed | tee logs/mnist_fourier_cnn_s$seed.txt
-    python -u -W ignore::UserWarning code/models/mnist_fourier_mlp.py $path $device $seed 5 | tee logs/mnist_fourier_mlp_s$seed.txt
+    python -u -W ignore::UserWarning code/models/mnist_fourier_mlp.py $path $device $seed $terms_mlp | tee logs/mnist_fourier_mlp_s$seed.txt
 
     python -u -W ignore::UserWarning code/models/mnist_noise_aug_cnn.py $path $device $seed | tee logs/mnist_noise_aug_cnn_s$seed.txt
     python -u -W ignore::UserWarning code/models/mnist_noise_baseline_cnn.py $path $device $seed | tee logs/mnist_noise_baseline_cnn_s$seed.txt
-    python -u -W ignore::UserWarning code/models/mnist_noise_fourier_mlp.py $path $device $seed 5 | tee logs/mnist_noise_fourier_mlp_s$seed.txt
+    python -u -W ignore::UserWarning code/models/mnist_noise_fourier_mlp.py $path $device $seed $terms_mlp | tee logs/mnist_noise_fourier_mlp_s$seed.txt
 
     python -u -W ignore::UserWarning code/models/qd-3_baseline_cnn.py $path $device $seed | tee logs/qd-3_baseline_cnn_s$seed.txt
     python -u -W ignore::UserWarning code/models/qd-3_aug_cnn.py $path $device $seed | tee logs/qd-3_aug_cnn_s$seed.txt
     python -u -W ignore::UserWarning code/models/qd-3_aug_mispredict_cnn.py $path $device $seed | tee logs/qd-3_aug_mispredict_cnn_s$seed.txt
     python -u -W ignore::UserWarning code/models/qd-3_fourier_cnn.py $path $device $seed | tee logs/qd-3_fourier_cnn_s$seed.txt
-    python -u -W ignore::UserWarning code/models/qd-3_fourier_mlp.py $path $device $seed 5 | tee logs/qd-3_fourier_mlp_s$seed.txt
+    python -u -W ignore::UserWarning code/models/qd-3_fourier_mlp.py $path $device $seed $terms_mlp | tee logs/qd-3_fourier_mlp_s$seed.txt
 
     python -u -W ignore::UserWarning code/models/qd-345_baseline_cnn.py $path $device $seed | tee logs/qd-345_baseline_cnn_s$seed.txt
     python -u -W ignore::UserWarning code/models/qd-345_aug_cnn.py $path $device $seed | tee logs/qd-345_aug_cnn_s$seed.txt
     python -u -W ignore::UserWarning code/models/qd-345_aug_mispredict_cnn.py $path $device $seed | tee logs/qd-345_aug_mispredict_cnn_s$seed.txt
     python -u -W ignore::UserWarning code/models/qd-345_fourier_cnn_largest.py $path $device $seed | tee logs/qd-345_fourier_cnn_largest_s$seed.txt
-    python -u -W ignore::UserWarning code/models/qd-345_fourier_gnn.py $path $device $seed 20 0.25 --deep --skip_conn | tee logs/qd-345_fourier_gnn_s$seed.txt
+    python -u -W ignore::UserWarning code/models/qd-345_fourier_gnn.py $path $device $seed $terms_gnn $gnn_width --deep --skip_conn | tee logs/qd-345_fourier_gnn_s$seed.txt
 done
